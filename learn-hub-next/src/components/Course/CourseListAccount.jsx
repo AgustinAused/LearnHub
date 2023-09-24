513d755aa80593900972d10c59b9df3856eed136
Reinapepeada <alejandrobiarrieta@gmail.com>
2023-09-24 17:18:59 -0300

    empezando las pestaña de servicios ofrecidos

--- learn-hub-next/src/components/Course/CourseListAccount.jsx
+++ learn-hub-next/src/components/Course/CourseListAccount.jsx
@@ -2,7 +2,7 @@ import CourseAccount from "../cards/CourseAccount";
 
 export default function CourseListAccount({courses}) {
     return(
-        <div className="">
+        <div className="flex-col m-6 space-y-5 w-3/4 ">
 
             { courses.map((course) => (
                 <CourseAccount course={course} />
