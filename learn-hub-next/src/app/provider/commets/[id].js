90e4519d7a5ac9e86fed4b7d7fc780c71d22fb97
Agustin Aused <agus.aused@gmail.com>
2023-09-29 20:39:27 -0300

    sistema de comentario

--- learn-hub-next/src/app/provider/commets/[id].js
+++ learn-hub-next/src/app/provider/commets/[id].js
@@ -7,7 +7,7 @@ export default function Comment({ Comments }) {
     return (
         <div>
             {/* aca irian los todos los comentarios de todos los cursos  */}
-            
+            <h2>Sistema de gestion de Comentario</h2>
             {Comments.map((comment) => (
                     <div key={comment.id}>
                         <CommentGest com={comment} />
