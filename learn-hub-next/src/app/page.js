0983d3d9b0952c135a162cd603f0a7028eac5a27
Reinapepeada <alejandrobiarrieta@gmail.com>
2023-09-29 20:36:29 -0300

    restaurando el page de la carpeta app, y deajdno el de generic user

---
+++ learn-hub-next/src/app/page.js
@@ -0,0 +1,30 @@
+import { CarouselCustomNavigation } from "@/components/carrousel/CarouselCustomNavigation";
+import { HorizontalCard } from "@/components/cards/HorizontalCard";
+import { BackgroundBlogCard } from "@/components/cards/BackgroundBlogCard";
+import { HorizontalCardinvert } from "@/components/cards/HorizontalCardInvert";
+import Encabezado from "@/components/Encabezado";
+
+export default function Home() {
+  return (
+    <main className="p-12">
+      <Encabezado />
+      <section className=" flex justify-center content-center">
+
+        <CarouselCustomNavigation />
+
+      </section>
+      
+      <section className="flex  flex-col my-8 justify-center items-center space-y-4 md:flex-row md:space-x-4 md:space-y-0">
+        <BackgroundBlogCard />
+        <BackgroundBlogCard />
+        <BackgroundBlogCard />
+      </section>
+
+      <section className="my-8 flex flex-col items-center space-y-4">
+        <HorizontalCard variant="gradient" />
+        <HorizontalCardinvert />
+        <HorizontalCard />
+      </section>
+    </main>
+  );
+}
