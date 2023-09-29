c5d4248d1078f03cbeb42f112232f394338d6fdb
Agustin Aused <agus.aused@gmail.com>
2023-09-29 20:48:03 -0300

    arreglo de rutas

--- learn-hub-next/src/components/Encabezado.jsx
+++ learn-hub-next/src/components/Encabezado.jsx
@@ -7,8 +7,7 @@ export default function Encabezado({titulo='Ofrece tu servicio con nosotros', su
           <div class="text-center text-black">
             <h1 class="text-6xl font-extrabold mb-6">{titulo}</h1>
             <h2 class="text-3xl font-semibold mb-8">{subtitulo}</h2>
-            <Link href="/sign-up"
-              
+            <Link href="/genericUser/sign-up"
               className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-3 px-8 rounded-full text-lg transition duration-300 ease-in-out transform hover:scale-105"
             >
               ¡Empieza Ahora!
