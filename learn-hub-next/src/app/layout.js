15b469bb4c78e08fa264cc1f380d8c2a87df3968
Reinapepeada <alejandrobiarrieta@gmail.com>
2023-09-29 22:30:56 -0300

    arreglo de una locura que no funcionaba

--- learn-hub-next/src/app/layout.js
+++ learn-hub-next/src/app/layout.js
@@ -2,7 +2,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import {NavbarWithMegaMenu} from "../components/navBars/NavMaterialT";
 import { FooterWithSocialLinks } from "@/components/footer/Footer";
-import { useState } from "react";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,13 +14,12 @@ export const metadata = {
 
 
 export default function RootLayout({ children }) {
-  const [isAuthenticated, setIsAuthenticated] = useState(false); 
   return (
     <html lang="en">
       <body >
         {/* para utilizar dos tipos de navbar dependiendo de si esta o no autenticado */}
-        {isAuthenticated ? <NavbarAutenticado /> : <NavbarNoAutenticado />}
-        {/* <NavbarWithMegaMenu /> */}
+        {/* {isAuthenticated ? <NavbarAutenticado /> : <NavbarNoAutenticado />} */}
+        <NavbarWithMegaMenu />
         <div className={inter.className}>{children}</div>
         <FooterWithSocialLinks />
         
--- learn-hub-next/src/components/Encabezado.jsx
+++ learn-hub-next/src/components/Encabezado.jsx
@@ -3,10 +3,10 @@ import Link from 'next/link'
 
 export default function Encabezado({titulo='Ofrece tu servicio con nosotros', subtitulo='Descubre el Poder de Aprender a Tu Manera en LearnHub'}) {
   return (
-    <div class=" h-screen flex items-center justify-center">
-          <div class="text-center text-black">
-            <h1 class="text-6xl font-extrabold mb-6">{titulo}</h1>
-            <h2 class="text-3xl font-semibold mb-8">{subtitulo}</h2>
+    <div className=" h-screen flex items-center justify-center">
+          <div className="text-center text-black">
+            <h1 className="text-6xl font-extrabold mb-6">{titulo}</h1>
+            <h2 className="text-3xl font-semibold mb-8">{subtitulo}</h2>
             <Link href="/genericUser/sign-up"
               className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-3 px-8 rounded-full text-lg transition duration-300 ease-in-out transform hover:scale-105"
             >
