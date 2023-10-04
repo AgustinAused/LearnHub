fa4e87aba66ffd897d91599327821a59795e622c
Agustin Aused <agus.aused@gmail.com>
2023-10-04 11:25:28 -0300

    Update page.js

--- learn-hub-next/src/app/genericUser/services/page.js
+++ learn-hub-next/src/app/genericUser/services/page.js
@@ -1,19 +1,13 @@
 'use client';
 import CourseList from '@/components/Course/CourseList'
+import CourseFilter from '@/components/filter/Filter';
 import FilterModal from '@/components/modal/FilterModal';
 import React,{useState} from 'react'
 
 export default function page() {
-    // const [isModalOpen, setIsModalOpen] = useState(false);
-
-    // const toggleModal = () => {
-    //     setIsModalOpen(!isModalOpen);
-    // };
-
     return (
         <div>
-              
-            <button onClick={toggleModal} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+            <button onClick={{/*toggleModal*/}} className="flex g-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
                 Filtrar
             </button>
             {/* <FilterModal isOpen={isModalOpen} onClose={toggleModal} /> */}
