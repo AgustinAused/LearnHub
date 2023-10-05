bbe6c742d0fdd1ca398bd75e1f2437ebd8bfae3f
Agustin Aused <agus.aused@gmail.com>
2023-10-04 23:31:14 -0300

    create page dinamics and comments

---
+++ learn-hub-next/src/app/provider/commets/[slug]/page.js
@@ -0,0 +1,36 @@
+'use client';
+import { useEffect,useState } from "react"
+import CommentGest from "@/components/comment/CommentGest";
+import coursesData from "@/data/coursesData";
+
+
+
+export default function page({ params }) {
+    const [commentsArr,setCommentsArr] = useState([]);
+    useEffect( () => {
+        const fetchData = async () => {
+        try {
+            console.log(params.slug);
+            let arrcourseDat = coursesData.filter(item => item.id == params.slug);
+            let courseDat = arrcourseDat[0];
+            setCommentsArr(courseDat.comentarios);
+            console.log(courseDat);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    };
+    fetchData();
+    }, []);
+    return (
+        <div>
+            {/* aca irian los todos los comentarios de todos los cursos  */}
+            <h2>Sistema de gestion de Comentario</h2>
+            {commentsArr.map((comment) => (
+                    <div key={comment.id}>
+                        <CommentGest com={comment} />
+                    </div>
+            ))}
+
+        </div>
+    )
+};
