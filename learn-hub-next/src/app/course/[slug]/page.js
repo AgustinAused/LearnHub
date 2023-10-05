c01d69d42e13e67a26db471738df2d4f10fd5076
Agustin Aused <agus.aused@gmail.com>
2023-10-04 23:31:35 -0300

    Update course pages

--- learn-hub-next/src/app/course/[slug]/page.js
+++ learn-hub-next/src/app/course/[slug]/page.js
@@ -1,7 +1,12 @@
+import ArticulCourse from '@/components/Course/ArticulCourse';
 import React from 'react'
 
 export default function page({params}) {
   return (
-    <div>{params.slug}</div>
-  )
+    <div className='my-28'>
+        {/* <ArticulCourse course={course}/> */}
+        <ArticulCourse course={params.slug} />
+    </div>
+  );
 }
+
