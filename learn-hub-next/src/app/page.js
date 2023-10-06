ec29116a81c652afac748224d6a98fa4205f8221
Agustin Aused <agus.aused@gmail.com>
2023-10-06 16:37:26 -0300

    update main page

--- learn-hub-next/src/app/page.js
+++ learn-hub-next/src/app/page.js
@@ -15,15 +15,15 @@ export default function Home() {
       </section>
       
       <section className="flex  flex-col my-8 justify-center items-center space-y-4 md:flex-row md:space-x-4 md:space-y-0">
-        <BackgroundBlogCard />
-        <BackgroundBlogCard />
-        <BackgroundBlogCard />
+        <BackgroundBlogCard parrafo={"LearnHub es brillante. Clases variadas, profesores apasionados y una experiencia fácil. Mi opción para aprender."} persona={"Elena García"} />
+        <BackgroundBlogCard parrafo={"¡Impresionante plataforma! Clases de alta calidad, profesores expertos y fácil de usar. Aprendizaje sin complicaciones."} persona={"Daniel Martínez"} />
+        <BackgroundBlogCard parrafo={"LearnHub es genial. Clases increíbles, profesionales dedicados y una interfaz sencilla. Recomiendo encarecidamente."} persona={"Laura Sánchez"} />
       </section>
 
       <section className="my-8 flex flex-col items-center space-y-4">
-        <HorizontalCard variant="gradient" />
-        <HorizontalCardinvert />
-        <HorizontalCard />
+        <HorizontalCard variant="gradient" title={"Variedad y Acceso a Expertos"} parah={"Una plataforma de marketplace de clases ofrece acceso a una amplia variedad de temas y disciplinas, desde idiomas y artes creativas hasta habilidades técnicas y académicas. Los estudiantes pueden elegir entre una gama diversa de profesores y expertos en sus campos, lo que les permite encontrar el mentor perfecto para sus necesidades específicas de aprendizaje."}/>
+        <HorizontalCardinvert title={"Flexibilidad y Conveniencia"} parah={"Estas plataformas suelen ofrecer flexibilidad en términos de horarios y ubicación. Los estudiantes pueden tomar clases desde la comodidad de sus hogares, adaptando sus horarios de aprendizaje para que se ajusten a sus agendas ocupadas. Esto es especialmente beneficioso para aquellos con compromisos laborales o académicos."}/>
+        <HorizontalCard title={"Comunidad y Retroalimentación"} parah={"Muchas plataformas de marketplace de clases fomentan la creación de una comunidad educativa en línea. Los estudiantes pueden interactuar entre sí, compartir experiencias y colaborar en proyectos. Además, los sistemas de retroalimentación y reseñas permiten a los estudiantes evaluar la calidad de las clases y de los profesores, ayudando a otros usuarios a tomar decisiones informadas sobre sus elecciones de aprendizaje."}/>
       </section>
     </main>
   );
