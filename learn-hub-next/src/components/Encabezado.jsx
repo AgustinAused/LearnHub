e697fa49d4a326979e0d10f46e38add20fe86d81
Agustin Aused <agus.aused@gmail.com>
2023-10-06 16:49:39 -0300

    Update Encabezado.jsx

--- learn-hub-next/src/components/Encabezado.jsx
+++ learn-hub-next/src/components/Encabezado.jsx
@@ -8,7 +8,7 @@ export default function Encabezado({titulo='Ofrece tu servicio con nosotros', su
             <h1 className="text-6xl font-extrabold mb-6">{titulo}</h1>
             <h2 className="text-3xl font-semibold mb-8">{subtitulo}</h2>
             <Link href="/genericUser/sign-up"
-              className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-3 px-8 rounded-full text-lg transition duration-300 ease-in-out transform hover:scale-105"
+              className="bg-black  text-white font-semibold py-3 px-8 rounded-full text-lg transition duration-300 ease-in-out transform hover:scale-105"
             >
               ¡Empieza Ahora!
             </Link>
