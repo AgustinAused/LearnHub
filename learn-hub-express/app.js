eb4886fd64ba703a01ed69e0aa364686c1c74990
Agustin Aused <agus.aused@gmail.com>
2023-10-30 22:15:16 -0300

    add new auth file and db connection

--- learn-hub-express/app.js
+++ learn-hub-express/app.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
+//incorporo cors
+const cors = require('cors');
 const port = 3000;
+
+app.use(cors());
+//aplico cors
+app.use(cors());
+app.use(cookieParser());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
