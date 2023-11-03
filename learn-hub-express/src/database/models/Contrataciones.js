a785ed2ab6aebff333af4892f2ca83a4d833bd39
Agustin Aused <agus.aused@gmail.com>
2023-11-03 13:47:49 -0300

    rollback file models

---
+++ learn-hub-express/src/database/models/Contrataciones.js
@@ -0,0 +1,14 @@
+let mongoose = require('mongoose')
+let mongoosePaginate = require('mongoose-paginate')
+let ContratacionSchema = new mongoose.Schema({
+    name: String,
+    apellido: String,
+    email: String,
+    telefono: String,
+    horario: String,
+    mensaje: String,
+    estado: String,
+})
+ContratacionSchema.plugin(mongoosePaginate)
+const Contratacion = mongoose.model('Contratacion', ContratacionSchema)
+module.exports = Contratacion;
