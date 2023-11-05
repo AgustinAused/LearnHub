e113115e1db72ac6fbc65f1b7964ef94c82e4634
Agustin Aused <agus.aused@gmail.com>
2023-11-05 04:35:26 -0300

    modify routers

--- learn-hub-express/src/routes/User-router.js
+++ learn-hub-express/src/routes/User-router.js
@@ -6,14 +6,13 @@ const Authorization = require('../auth/authentication');
 
 //User router
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('Llegaste a la ruta de  api/user.routes');
+router.get('/', function(req, res) {
+    res.send('Llegaste a la ruta de  api/user-routes');
 });
-// router.post('/registration', UserController.createUser) 
-// router.post('/login/', UserController.loginUser)
+router.post('/registration', UserController.createUser); 
+router.post('/login/', UserController.loginUser);
 router.get('/users',Authorization, UserController.getUsers); //Get all the user
 router.post('/userByMail', Authorization, UserController.getUsersByMail); //Get user by mail
-router.post('/createUser',Authorization,UserController.createUser); //create User
 router.put('/update', Authorization, UserController.updateUser); //Update user 
 router.delete('/delete/:id', Authorization, UserController.deleteUserById); //Delete User
 
