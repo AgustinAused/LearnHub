4530ddc23b2f3f48b37998454fba37b9ef52f7dc
Reinapepeada <alejandrobiarrieta@gmail.com>
2023-11-10 02:37:29 -0300

    correcion en serviceRouter

--- learn-hub-express/src/routers/Service-router.js
+++ learn-hub-express/src/routers/Service-router.js
@@ -5,10 +5,10 @@ const Service_controller = require('../controllers/Service-controller');
 
 //Service Router
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('Llegaste a la ruta de  api/service-router');
-});
-router.get('/services',  Service_controller.getService); //Get all the services
+// router.get('/', function(req, res, next) {
+//     res.send('Llegaste a la ruta de  api/service-router');
+// });
+router.get('/allServices',  Service_controller.getAllServices); //Get all the services
 
 
 module.exports = router;
