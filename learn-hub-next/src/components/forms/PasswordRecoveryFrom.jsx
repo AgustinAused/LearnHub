b4631c29a16076fce5c3da1484693d211dba664c
Agustin Aused <agus.aused@gmail.com>
2023-11-17 18:16:35 -0300

    integracion con el back para enviar el email

--- learn-hub-next/src/components/forms/PasswordRecoveryFrom.jsx
+++ learn-hub-next/src/components/forms/PasswordRecoveryFrom.jsx
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 import {
   Card,
   Input,
-  Checkbox,
   Button,
   Typography,
 } from "@material-tailwind/react";
@@ -12,16 +11,31 @@ const PasswordRecoveryForm = () => {
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Aquí puedes implementar la lógica para enviar correos electrónicos de recuperación
-    // o restablecer contraseñas usando el correo electrónico proporcionado (almacenado en la variable 'email').
-    // Por ahora, solo mostraremos un mensaje de éxito ficticio.
-    setMessage(`Se ha enviado un correo electrónico de recuperación a ${email}`);
+    //
+    fetch(`http//localhost:4050/api/users/sendResetEmail`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, name }),
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      alert("Se ha enviado el enlace de restablecimiento de contraseña");
+    })
+    .catch((err) => {
+      console.log(err)
+      alert("No se ha podido enviar el enlace de restablecimiento de contraseña");
+    });
   };
     return (
       <Card color="white" className='p-6 my-36' shadow={true}>
@@ -31,7 +45,7 @@ const PasswordRecoveryForm = () => {
         <Typography color="gray" className="mt-1 font-normal">
           ingresar su correo electrónico y nombre 
         </Typography>
-        <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+        <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96" onSubmit={handleSubmit}>
           <div className="mb-4 flex flex-col gap-6">
             <Input size="lg" label="Name" />
             <Input size="lg" label="Email" />
