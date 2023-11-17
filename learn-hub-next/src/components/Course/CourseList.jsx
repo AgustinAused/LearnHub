f1c08700f0d9b0723f339c12da2de3994b38ad55
Agustin Aused <agus.aused@gmail.com>
2023-11-17 15:49:59 -0300

    Frontend-Backend integration for fetching all courses

--- learn-hub-next/src/components/Course/CourseList.jsx
+++ learn-hub-next/src/components/Course/CourseList.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React,{useEffect} from "react";
 import CardCourse from "@/components/cards/CardCourse";
-import ListCourse from "@/data/coursesData"
-//props 
+// import ListCourse from "@/data/coursesData"
+
 export default function CourseList() {
+    const [listCourse, setListCourse] = useState([]);
+
+    useEffect(() => {
+        // Realizar la solicitud fetch en useEffect para evitar llamadas continuas
+        fetch('http://localhost:4050/api/service/allServices', {
+            method: 'GET',
+            headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            // Recuperamos el array de cursos
+            const courses = data.cursos;
+            // Actualizamos el estado con el array de cursos
+            setListCourse(courses);
+        })
+        .catch(error => console.error('Error fetching data:', error));
+    },[]); 
+
     return (
         <div className="m-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-4">
-            { ListCourse.map((course) => (
+            { listCourse.map((course) => (
                 <CardCourse course={course} />
             ))}
         </div>
