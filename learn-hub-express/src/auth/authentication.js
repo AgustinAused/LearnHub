99f9fd03079bbea3c4508909bd45201d652fbf89
Agustin Aused <agus.aused@gmail.com>
2023-11-29 17:20:17 -0300

    Update authentication.js

--- learn-hub-express/src/auth/authentication.js
+++ learn-hub-express/src/auth/authentication.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 let authorization = function (req, res, next) {
 
-    const token = req.headers.authorization?.split(" ")[1];;
+    const token = req.headers.authorization?.split(" ")[1];
     console.log("token",token);
     let msg = {auth: false, message: 'No token provided.'};
     if (!token){
