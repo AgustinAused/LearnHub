ef1105610627450883528d0947073b7e001d6540
Agustin Aused <agus.aused@gmail.com>
2023-12-05 17:02:27 -0300

    agregue el formulario para que el usuario modifique su perfil

--- learn-hub-next/src/app/provider/account/edit/page.js
+++ learn-hub-next/src/app/provider/account/edit/page.js
@@ -1,10 +1,44 @@
+"use client";
+import GetUserDetails from "@/actions/GetUserDetails";
+import FormModifyAccount from "@/components/forms/FormModifyAccount";
+import { useEffect, useState } from "react";
+// import Loading from "./loading";
+
 export default function page(params) {
+    const [user, setUser] = useState({});
+    const [loading, setLoading] = useState(true);
     
 
+    useEffect(() => {
+        const fetchUserDetails = async () => {
+            try {
+                const userDetails = await GetUserDetails();
+                console.log("User details: ", userDetails);
+                setUser(userDetails);
+            } catch (error) {
+                console.error("Error fetching user details: ", error);
+            } finally {
+                // Marcar que la carga ha terminado, independientemente de si fue exitosa o no
+                setLoading(false);
+            }
+        };
+        fetchUserDetails();
+        }, []);
+
+    const handlerSubmit = (newAccount) => {
+        console.log("New account: ", newAccount);
+    };
+
     return (
-        <div>
-            
-            
-        </div>
+    <div className="max-w-md mx-auto mt-10 p-4">
+        <h1 className="text-4xl font-bold mb-6">Account Edit Page</h1>
+        {/* Aquí puedes agregar el resto de tu contenido */}
+        {loading ? (
+                <p className="text-center">Cargando...</p>
+            ) : (
+                <FormModifyAccount onGuardarCambios={handlerSubmit} account={user} />
+            )
+            }
+    </div>
     )
 };
--- learn-hub-next/src/components/forms/FormModifyAccount.jsx
+++ learn-hub-next/src/components/forms/FormModifyAccount.jsx
@@ -6,9 +6,90 @@ export default function FormModifyAccount({account ,onGuardarCambios}) {
         email: account.email,
         degree: account.degree,
         expirience: account.expirience,
+    });
+
+    const handleChange = (event) => {
         
-      });
-
-
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value,
+        });
+    };
     
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+        // Aquí puedes realizar alguna acción con los datos, como enviarlos a la API o realizar otra lógica.
+        onGuardarCambios(formData);
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className="max-w-md mx-auto bg-white p-8 shadow-md rounded-md mt-10">
+    <div className="form-group">
+        <label htmlFor="name" className="block text-sm font-medium text-gray-600">
+            Nombre
+        </label>
+        <input
+            type="text"
+            className="mt-1 p-2 w-full border rounded-md form-control"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+        />
+    </div>
+    <div className="form-group">
+        <label htmlFor="email" className="block text-sm font-medium text-gray-600">
+            Correo electrónico
+        </label>
+        <input
+            type="email"
+            className="mt-1 p-2 w-full border rounded-md form-control"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+        />
+    </div>
+    <div className="form-group">
+        <label htmlFor="degree" className="block text-sm font-medium text-gray-600">
+            Grado académico
+        </label>
+        <input
+            type="text"
+            className="mt-1 p-2 w-full border rounded-md form-control"
+            id="degree"
+            name="degree"
+            value={formData.degree}
+            onChange={handleChange}
+            required
+        />
+    </div>
+    <div className="form-group">
+        <label htmlFor="experience" className="block text-sm font-medium text-gray-600">
+            Experiencia
+        </label>
+        <textarea
+            className="mt-1 p-2 w-full border rounded-md form-control"
+            id="experience"
+            name="expirience"
+            rows="3"
+            value={formData.expirience}
+            onChange={handleChange}
+            required
+        ></textarea>
+    </div>
+    <div className="flex items-center justify-end mt-6">
+        <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded focus:outline-none focus:shadow-outline-blue btn btn-primary"
+        >
+            Guardar Cambios
+        </button>
+    </div>
+</form>
+
+    );
 };
