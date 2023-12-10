7eccf909f1cc542515b1738035b71e8cad873459
Agustin Aused <agus.aused@gmail.com>
2023-12-10 18:44:22 -0300

    add new actions

---
+++ learn-hub-next/src/actions/PublishComments.jsx
@@ -0,0 +1,32 @@
+'use server';
+
+import { cookies } from "next/headers";
+
+export default async function PublishComments({commentId, serviceId}) {
+    const tokenN = cookies().get("token");
+    // Convert token to string
+    let tokenString = JSON.stringify(tokenN);
+    const extractedToken = tokenString.split('"')[7];
+    console.log(commentId)
+    try {
+        const response =  await fetch(`http://localhost:4050/api/comments/changeState`, {
+            method: "PUT",
+            headers: {
+                Authorization: `Bearer ${extractedToken}`,
+                "Content-Type": "application/json", // Specify the content type
+            },
+            body: JSON.stringify({ commentId, serviceId }), // JSON.stringify
+        });
+        console.log("Full Response:", response); 
+        const data = await response.json();
+        console.log("Parsed JSON Data:", data);
+
+        // return null;
+        return data;
+    } catch (error) {
+        console.error("Error:", error);
+        // You might want to handle the error here, depending on your use case
+        throw error;
+    }
+    
+};
