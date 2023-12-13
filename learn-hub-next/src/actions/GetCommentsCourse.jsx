4e7361c484b99517061ab129ee507ebfb2076402
Reinapepeada <alejandrobiarrieta@gmail.com>
2023-12-12 22:11:12 -0300

    arreglando la informacion del curso en su pagina
    estabamos pasando el token sin necesidad

--- learn-hub-next/src/actions/GetCommentsCourse.jsx
+++ learn-hub-next/src/actions/GetCommentsCourse.jsx
@@ -6,16 +6,15 @@ import { cookies } from "next/headers";
 export default async function GetCommentsCourse(id) {
     console.log(id);
     try{
-        const token = cookies().get('token');
-        // Convert token to string
-        let tokenString = JSON.stringify(token);
-        const extractedToken = tokenString.split('"')[7];
+        // const token = cookies().get('token');
+        // // Convert token to string
+        // let tokenString = JSON.stringify(token);
+        // const extractedToken = tokenString.split('"')[7];
         // console.log(token);
         const res = await fetch(`http://localhost:4050/api/comments/all?id=${id}`, {
             method: "GET",
             headers: {
                 'Content-Type' : 'application/json',
-                'authorization': `Bearer ${extractedToken}`,
             }
         });
         if (!res.ok) {
