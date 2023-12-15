0c9efea4efeb4a1a975c31984f7f64d49c37acad
Agustin Aused <agus.aused@gmail.com>
2023-12-15 20:09:50 -0300

    fix warning in manager Comments Page
    Signed-off-by: Agustin Aused <agus.aused@gmail.com>

--- learn-hub-next/src/app/provider/commets/[slug]/page.js
+++ learn-hub-next/src/app/provider/commets/[slug]/page.js
@@ -32,7 +32,7 @@ export default function page({ params }) {
             <h3 className="text-2xl md:text-3xl lg:text-4xl font-bold text-center mt-8 mb-4 text-gray-800">Curso: {course}</h3>
             {commentsArr.length > 0 ? (
     commentsArr.map((comment) => (
-        <div key={comment.id}>
+        <div key={comment._id}>
             <CommentGest com={comment}  serviceId={params.slug}/>
         </div>
     ))
